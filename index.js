221c18ebaa053af7f2c159284744a3f08cd58ee1
harsh29patel <harshpatel290703@gmail.com>
2024-04-15 12:36:14 +0530

    git hub data addded

--- index.js
+++ index.js
@@ -53,10 +53,9 @@ app.get('/twitter', (req,res)=>{
  app.get('/youtube',(req,res)=>{
     res.send('<h1> Chai Aur Code</h1>')
  })
- app.get('/github',(req,res)=>{
-   res.json(githuData)
- })
-
+  app.get('/github',(req,res)=>{
+    res.json(githuData)
+  })
     app.listen(process.env.PORT, () => {
     console.log(`Example app listening on port ${port}`)
 })
